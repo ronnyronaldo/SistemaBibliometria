f2f029a9612666ba4f96f09e4a4fbf9395d4f475
Tania <tania72landi@gmail.com>
2021-04-22 00:37:45 -0500

    diseñado plantilla base

--- frontsistemabibliometria/src/routes.js
+++ frontsistemabibliometria/src/routes.js
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import Dashboard from "views/Dashboard.js";
 import UserProfile from "views/UserProfile.js";
 import TableList from "views/TableList.js";
@@ -26,14 +9,6 @@ import Upgrade from "views/Upgrade.js";
 
 const dashboardRoutes = [
   {
-    upgrade: true,
-    path: "/upgrade",
-    name: "Upgrade to PRO",
-    icon: "nc-icon nc-alien-33",
-    component: Upgrade,
-    layout: "/admin",
-  },
-  {
     path: "/dashboard",
     name: "Dashboard",
     icon: "nc-icon nc-chart-pie-35",
@@ -41,33 +16,34 @@ const dashboardRoutes = [
     layout: "/admin",
   },
   {
-    path: "/user",
-    name: "User Profile",
-    icon: "nc-icon nc-circle-09",
+    path: "/publicaciones",
+    name: "Publicaciones",
+    icon: "nc-icon nc-single-copy-04",
     component: UserProfile,
     layout: "/admin",
   },
   {
-    path: "/table",
-    name: "Table List",
-    icon: "nc-icon nc-notes",
+    path: "/revistas",
+    name: "Revistas",
+    icon: "nc-icon nc-paper-2",
     component: TableList,
     layout: "/admin",
   },
   {
-    path: "/typography",
-    name: "Typography",
-    icon: "nc-icon nc-paper-2",
+    path: "/estadisticasProveedores",
+    name: "Est. Proveedores",
+    icon: "nc-icon nc-notes",
     component: Typography,
     layout: "/admin",
   },
-  {
+  /*{
     path: "/icons",
     name: "Icons",
     icon: "nc-icon nc-atom",
     component: Icons,
     layout: "/admin",
   },
+  /*
   {
     path: "/maps",
     name: "Maps",
@@ -81,7 +57,7 @@ const dashboardRoutes = [
     icon: "nc-icon nc-bell-55",
     component: Notifications,
     layout: "/admin",
-  },
+  },*/
 ];
 
 export default dashboardRoutes;
