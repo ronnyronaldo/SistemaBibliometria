5bb4813ea78bc6f30e89957a987d204585d59be1
ronnyronaldo <ronnyrendon28@gmail.com>
2021-04-24 20:04:39 -0500

    get para scopus

--- projects/api/api.py
+++ projects/api/api.py
@@ -1,38 +1,52 @@
 import flask
 from flask import request, jsonify
+from flaskext.mysql import MySQL
 
 app = flask.Flask(__name__)
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'bd_tesis'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
 app.config["DEBUG"] = True
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
-
+# Get principal para ver y documentar el estado de la API
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+    return '''<h1>API REST en Flask para desarrolo de tesis bibliometria</h1>
+<p>A Estado de la API: metodo get y get con filtrado por id de articulo.</p>'''
 
+# Get para recuperar todos los articulos
+@app.route('/scopus', methods=['GET'])
+def scopus():
+    cur = mysql.connect().cursor()
+    cur.execute('''select * from bd_tesis.scopus''')
+    r = [dict((cur.description[i][0], value)
+                for i, value in enumerate(row)) for row in cur.fetchall()]
+    return jsonify({'Articulos de Scopus' : r})
 
-# A route to return all of the available entries in our catalog.
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
-
+# Get para recuper un articulo mediante un id
+@app.route('/scopus/busqueda', methods=['GET'])
+def api_busqueda():
+    # Con esto se filtrara para obtener un articulo por su id.
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No se ha especificado por que parametro desea buscar. Por Favor ingrese ej id=1."
+    # Recorremos toda nuestra base primero
+    cur = mysql.connect().cursor()
+    cur.execute('''select * from bd_tesis.scopus''')
+    bd = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+    # Se crea una lista vacia para los resultados obtenidos
+    resultados = []
+    # El id es unico pero se retornan todos los elemnetos con ese id
+    for id_ar in bd:
+        if id_ar['id_article'] == id:
+            resultados.append(id_ar)
+    # Con jsonify convertimos nuestro diccionario en JSON
+    return jsonify(resultados)
 app.run()
