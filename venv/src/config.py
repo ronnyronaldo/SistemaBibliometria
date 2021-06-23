2e05bac213ab8fbb14a43754440641c9f4aa026c
Tania <tania72landi@gmail.com>
2021-06-22 19:04:47 -0500

    cambio de base

--- venv/src/config.py
+++ venv/src/config.py
@@ -5,10 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/bd_tesis'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/bd_bibliometria'
 # Turn off the Flask-SQLAlchemy event system and warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 MYSQL_DATABASE_USER = 'root'
 MYSQL_DATABASE_PASSWORD = ''
-MYSQL_DATABASE_DB= 'bd_tesis'
+MYSQL_DATABASE_DB= 'bd_bibliometria'
 MYSQL_DATABASE_HOST = 'localhost'
