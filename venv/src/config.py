2f03899404ae94e2a10be2c93b768523dfcc70d9
Tania <tania72landi@gmail.com>
2021-08-26 12:41:00 -0500

    Conexion de la base de datos en la nube

--- venv/src/config.py
+++ venv/src/config.py
@@ -5,6 +5,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 # Connect to the database
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admintl@servidormysqltl:tania123.@servidormysqltl.mysql.database.azure.com:3306/bd_bibliometria'
+
+# Turn off the Flask-SQLAlchemy event system and warning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+MYSQL_DATABASE_USER = 'admintl@servidormysqltl'
+MYSQL_DATABASE_PASSWORD = 'tania123.'
+MYSQL_DATABASE_DB= 'bd_bibliometria'
+MYSQL_DATABASE_HOST = 'servidormysqltl.mysql.database.azure.com'
+#SQLALCHEMY_POOL_SIZE = 10
+#SQLALCHEMY_POOL_TIMEOUT = 300
+SQLALCHEMY_MAX_OVERFLOW = -1
+"""
+# Connect to the database
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/bd_bibliometria'
 # Turn off the Flask-SQLAlchemy event system and warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -14,4 +27,4 @@ MYSQL_DATABASE_DB= 'bd_bibliometria'
 MYSQL_DATABASE_HOST = 'localhost'
 #SQLALCHEMY_POOL_SIZE = 10
 #SQLALCHEMY_POOL_TIMEOUT = 300
-SQLALCHEMY_MAX_OVERFLOW = -1
+SQLALCHEMY_MAX_OVERFLOW = -1"""
