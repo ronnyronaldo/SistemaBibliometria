3e20325a962c8db89b046eff75636ed17e34f6df
Tania <tania72landi@gmail.com>
2021-11-02 17:06:47 -0500

    Mejoras en la seccion de parámetros

--- frontsistemabibliometria/src/views/Parametro.js
+++ frontsistemabibliometria/src/views/Parametro.js
@@ -5,6 +5,7 @@ import { parametroService } from "_services/parametro.service";
 import { validacionInputService } from '../_services/validacionInput.service';
 import { Link } from "react-router-dom";
 
+
 // react plugin for creating notifications over the dashboard
 import NotificationAlert from "react-notification-alert";
 
@@ -40,35 +41,15 @@ function Parametro() {
   const [showModal, setShowModal] = React.useState(false);
   const [modalIsOpen, setModalIsOpen] = React.useState(false);
   const [parametroObj, setParametroObj] = React.useState({
-    id_parametro:0, 
-    nombre: "", 
+    id_parametro: 0,
+    nombre: "",
     valor: "",
     codigo_parametro: ""
-  })
-
+  });
+  const [idEliminarParametro, setIdEliminarParametro] = React.useState('');
+  const [modalIsOpenEliminar, setModalIsOpenEliminar] = React.useState(false);
   const notificationAlertRef = React.useRef(null);
   const notify = (place, mensaje, type) => {
-    //var color = Math.floor(Math.random() * 5 + 1);
-    //var type = "danger";
-    /*switch (color) {
-      case 1:
-        type = "primary";
-        break;
-      case 2:
-        type = "success";
-        break;
-      case 3:
-        type = "danger";
-        break;
-      case 4:
-        type = "warning";
-        break;
-      case 5:
-        type = "info";
-        break;
-      default:
-        break;
-    }*/
     var options = {};
     options = {
       place: place,
@@ -106,10 +87,11 @@ function Parametro() {
     setLoading(true);
     parametroService.eliminar(id_parametro).then(value => {
       setLoading(false);
-      if(value.respuesta.error == "False"){
+      closeModalEliminar();
+      if (value.respuesta.error == "False") {
         handleListaParametro();
         notify("tr", value.respuesta.valor, "primary");
-      }else{
+      } else {
         notify("tr", value.respuesta.valor, "danger");
       }
     })
@@ -119,31 +101,31 @@ function Parametro() {
     let nombre = document.getElementById("nombreText").value;
     let valor = document.getElementById("valorText").value;
     let codigo_parametro = document.getElementById("codigoParametroText").value;
-   
-    if(validacionInputService.campoVacio(nombre)){
-      if(validacionInputService.campoVacio(valor)){
-        if(validacionInputService.campoVacio(codigo_parametro)){
+
+    if (validacionInputService.campoVacio(nombre)) {
+      if (validacionInputService.campoVacio(valor)) {
+        if (validacionInputService.campoVacio(codigo_parametro)) {
           setLoading(true);
           parametroService.insertar({
             "nombre": nombre,
             "valor": valor,
             "codigo_parametro": codigo_parametro
-          }).then(value =>{
+          }).then(value => {
             setLoading(false);
-            if(value.respuesta.error == "False"){
+            if (value.respuesta.error == "False") {
               handleListaParametro();
               notify("tr", value.respuesta.valor, "primary");
-            }else{
+            } else {
               notify("tr", value.respuesta.valor, "danger");
             }
           })
-        }else{
+        } else {
           notify("tr", 'No ha ingresado el código del parámetro.', "danger");
         }
-      }else{
+      } else {
         notify("tr", 'No ha ingresado el valor del parámetro.', "danger");
       }
-    }else{
+    } else {
       notify("tr", 'No ha ingresado el nombre del parámetro.', "danger");
     }
   }
@@ -157,38 +139,45 @@ function Parametro() {
 
   function handleCargarDetalleParametro(id_parametro, nombre, valor, codigo_parametro) {
     setParametroObj({
-      id_parametro:id_parametro, 
-      nombre: nombre, 
+      id_parametro: id_parametro,
+      nombre: nombre,
       valor: valor,
       codigo_parametro: codigo_parametro
     });
     openModal()
   }
-  
+
   function actualizarParametro() {
     let id_parametro = parametroObj.id_parametro;
     let nombre = document.getElementById("nombreDetText").value;
     let valor = document.getElementById("valorDetText").value;
     let codigo_parametro = document.getElementById("codigoParametroDetText").value;
 
-    if(validacionInputService.campoVacio(nombre) && validacionInputService.campoVacio(valor) && validacionInputService.campoVacio(codigo_parametro)){
+    if (validacionInputService.campoVacio(nombre) && validacionInputService.campoVacio(valor) && validacionInputService.campoVacio(codigo_parametro)) {
       parametroService.actualizar({
         "id_parametro": id_parametro,
         "nombre": nombre,
         "valor": valor,
         "codigo_parametro": codigo_parametro
       }).then(value => {
-        if(value.respuesta.error== "False"){
+        if (value.respuesta.error == "False") {
           notify("tr", value.respuesta.valor, "primary");
           handleListaParametro();
         }
         closeModal();
       })
-    }else{
+    } else {
       notify("tr", 'Existen campos sin llenar.', "danger");
     }
   }
+  function closeModalEliminar() {
+    setModalIsOpenEliminar(false);
+  }
 
+  function openModalEliminar(id_parametro) {
+    setIdEliminarParametro(id_parametro);
+    setModalIsOpenEliminar(true);
+  }
   React.useEffect(() => {
     handleListaParametro();
   }, []);
@@ -202,12 +191,22 @@ function Parametro() {
         <Row>
           <Col md="12">
             <Card className="strpied-tabled-with-hover">
-            <Card.Header>
+              <Card.Header>
                 <Card.Title as="h4">Parámetro</Card.Title>
                 <p className="card-category">
                   Características Específicas del Sistema
                 </p>
                 <Row>
+                  <Col className="pr-1" md="4">
+                    <Form.Group>
+                      <label>CÓDIGO PARÁMETRO</label>
+                      <Form.Control
+                        id="codigoParametroText"
+                        defaultValue=""
+                        type="text"
+                      ></Form.Control>
+                    </Form.Group>
+                  </Col>
                   <Col className="pr-1" md="3">
                     <Form.Group>
                       <label>NOMBRE</label>
@@ -223,16 +222,6 @@ function Parametro() {
                       <label>VALOR</label>
                       <Form.Control
                         id="valorText"
-                        defaultValue=""
-                        type="text"
-                      ></Form.Control>
-                    </Form.Group>
-                  </Col>
-                  <Col className="pr-1" md="4">
-                    <Form.Group>
-                      <label>CÓDIGO PARÁMETRO</label>
-                      <Form.Control
-                        id="codigoParametroText"
                         defaultValue=""
                         type="text"
                       ></Form.Control>
@@ -255,20 +244,24 @@ function Parametro() {
                 <table className="table table-bordered table-hover" id="dataTableParametro" width="100%" cellSpacing="0">
                   <thead className="thead-dark">
                     <tr>
+                      <th>CÓDIGO PARÁMETRO</th>
                       <th>NOMBRE</th>
                       <th>VALOR</th>
-                      <th>CÓDIGO PARÁMETRO</th>
                       <th>ACCIONES</th>
                     </tr>
                   </thead>
                   <tbody>
                     {parametros.map(item => (
                       <tr className="small" key={item.id_parametro}>
-                        <td>{item.nombre}</td>
-                        <td>{item.valor}</td>
-                        <td>{item.codigo_parametro}</td>
-                        <td width="5%"><Link to="#" id="actualizarParametro" className="link col-sm-12 col-md-3" onClick={() => handleCargarDetalleParametro(item.id_parametro, item.nombre, item.valor, item.codigo_parametro)} ><i className="fas fa-pen-square fa-2x"></i></Link>
-                        <Link to="#" id="eliminarParametro" className="link col-sm-12 col-md-3" onClick={()=>handleEliminarParametro(item.id_parametro)}><i className="fas fa-trash-alt fa-2x"></i></Link></td>
+                        <td width="25%">{item.codigo_parametro}</td>
+                        <td width="25%">{item.nombre}</td>
+                        <td width="25%">{item.valor}</td>
+                        <td width="5%">
+                          <div class="btn-group-vertical" role="group" aria-label="Basic example" size={10}>
+                            <Button id="actualizarParametro" className="btn btn-sm active" type="button" variant="info" onClick={() => handleCargarDetalleParametro(item.id_parametro, item.nombre, item.valor, item.codigo_parametro)} >Editar</Button>
+                            <Button id="eliminarParametro" className="btn-sm active" type="button" variant="danger" onClick={() => openModalEliminar(item.id_parametro)}>Eliminar</Button>
+                          </div>
+                        </td>
                       </tr>
                     ))}
                   </tbody>
@@ -279,66 +272,102 @@ function Parametro() {
         </Row>
       </Container>
       <Modal
-          className="modal modal-primary"
-          show={modalIsOpen}
-        >
-          <Modal.Header className="justify-content-center">
-            <div className="modal-profile">
-              <i className="nc-icon nc-grid-45"></i>
-            </div>
-          </Modal.Header>
-          <Modal.Body className="text-center">
-            <p>Actualizar Parámetro</p>
-          </Modal.Body>
-          <div className="modal-footer">
-            <Col className="pr-1" md="12">
-              <Form.Group>
-                <label>NOMBRE</label>
-                <Form.Control
-                  id="nombreDetText"
-                  defaultValue={parametroObj.nombre}
-                  type="text"
-                ></Form.Control>
-              </Form.Group>
-            </Col>
-            <Col className="pr-1" md="12">
-              <Form.Group>
-                <label>VALOR</label>
-                <Form.Control
-                  id="valorDetText"
-                  defaultValue={parametroObj.valor}
-                  type="text"
-                ></Form.Control>
-              </Form.Group>
-            </Col>
-            <Col className="pr-1" md="12">
-              <Form.Group>
-                <label>CÓDIGO PARÁMETRO</label>
-                <Form.Control
-                  id="codigoParametroDetText"
-                  defaultValue={parametroObj.codigo_parametro}
-                  type="text"
-                ></Form.Control>
-              </Form.Group>
-            </Col>
-            <Button
-              className="btn-simple"
-              type="button"
-              variant="link"
-              onClick={() => closeModal()}
-            >
-              Regresar
-            </Button>
-            <Button
-              className="btn-simple"
-              type="button"
-              variant="link"
-              onClick={() => actualizarParametro()}
-            >
-              Grabar
-            </Button>
+        className="modal modal-primary"
+        show={modalIsOpen}
+      >
+        <Modal.Header className="justify-content-center">
+          <div className="modal-profile">
+            <i className="nc-icon nc-grid-45"></i>
           </div>
-        </Modal>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+          <p>Actualizar Parámetro</p>
+        </Modal.Body>
+        <div className="modal-footer">
+          <Col className="pr-1" md="12">
+            <Form.Group>
+              <label>CÓDIGO PARÁMETRO</label>
+              <Form.Control
+                id="codigoParametroDetText"
+                defaultValue={parametroObj.codigo_parametro}
+                type="text"
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+          <Col className="pr-1" md="12">
+            <Form.Group>
+              <label>NOMBRE</label>
+              <Form.Control
+                id="nombreDetText"
+                defaultValue={parametroObj.nombre}
+                type="text"
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+          <Col className="pr-1" md="12">
+            <Form.Group>
+              <label>VALOR</label>
+              <Form.Control
+                id="valorDetText"
+                defaultValue={parametroObj.valor}
+                type="text"
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+          <Button
+            id="regresar"
+            className="btn active"
+            type="button"
+            variant="secondary"
+            onClick={() => closeModal()}
+          >
+            Regresar
+          </Button>
+          <Button
+            id="actualizar"
+            className="btn active"
+            type="button"
+            variant="secondary"
+            onClick={() => actualizarParametro()}
+          >
+            Grabar
+          </Button>
+        </div>
+      </Modal>
+      <Modal
+        className="modal modal-primary"
+        show={modalIsOpenEliminar}
+        size="xl"
+      >
+        <Modal.Header className="justify-content-center">
+          <div className="modal-profile">
+            <i className="nc-icon nc-single-copy-04"></i>
+          </div>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+          <p>¿ Esta seguro de eliminar el parámetro ?</p>
+        </Modal.Body>
+        <div className="modal-footer">
+          <Button
+            id="regresar"
+            className="btn active"
+            type="button"
+            variant="secondary"
+            onClick={() => closeModalEliminar()}
+          >
+            Regresar
+          </Button>
+          <Button
+            id="eliminar"
+            className="btn active"
+            type="button"
+            variant="secondary"
+            onClick={() => handleEliminarParametro(idEliminarParametro)}
+          >
+            SI
+          </Button>
+        </div>
+      </Modal>
     </>
   );
 }
